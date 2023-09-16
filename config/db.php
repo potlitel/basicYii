4a48f2a99cf0eea53bcd8f683a84b098e57e97d9
potlitel <potlitel@gmail.com>
2023-09-16 12:04:18 -0400

    Trabajar con Bases de Datos.

--- config/db.php
+++ config/db.php
@@ -2,9 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    // 'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'sqlsrv:server=localhost;database=yii2basic',
+    'username' => 'sa',
+    'password' => 'zaqWE*123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
